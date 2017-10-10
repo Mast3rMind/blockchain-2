06e4882fe3266fe3a4cf5471b90154013aa962af
Praveen Yadav <pyadav@missingstack.com>
2017-10-11 02:21:35 +0530

    Added Addresses by replacing name

--- block.go
+++ block.go
@@ -5,7 +5,6 @@
 //    - Hash (Hash of its own block)
 //    - Hash of previous block (Hash of prev block)
 //  as a block header and data.
-
 package main
 
 import (
@@ -16,6 +15,7 @@ import (
 	"time"
 )
 
+// Block represents a block in the blockchain
 type Block struct {
 	Timestamp     int64
 	Transactions  []*Transaction
@@ -24,10 +24,37 @@ type Block struct {
 	Nonce         int
 }
 
-// We have to use a mechanism of converting
-// a Go struct into a byte array and restoring it back from a byte array
-//  use encoding/gob or JSON, XML, Protocol Buffers
+// NewBlock creates and returns Block
+func NewBlock(transactions []*Transaction, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), transactions, prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
 
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
+	return block
+}
+
+// NewGenesisBlock creates and returns genesis Block
+func NewGenesisBlock(coinbase *Transaction) *Block {
+	return NewBlock([]*Transaction{coinbase}, []byte{})
+}
+
+// HashTransactions returns a hash of the transactions in the block
+func (b *Block) HashTransactions() []byte {
+	var txHashes [][]byte
+	var txHash [32]byte
+
+	for _, tx := range b.Transactions {
+		txHashes = append(txHashes, tx.Hash())
+	}
+	txHash = sha256.Sum256(bytes.Join(txHashes, []byte{}))
+
+	return txHash[:]
+}
+
+// Serialize serializes the block
 func (b *Block) Serialize() []byte {
 	var result bytes.Buffer
 	encoder := gob.NewEncoder(&result)
@@ -40,6 +67,7 @@ func (b *Block) Serialize() []byte {
 	return result.Bytes()
 }
 
+// DeserializeBlock deserializes a block
 func DeserializeBlock(d []byte) *Block {
 	var block Block
 
@@ -50,40 +78,4 @@ func DeserializeBlock(d []byte) *Block {
 	}
 
 	return &block
-}
-
-// Hash calcualtion - it should be computationally difficult operation
-// to compute a hash to makes hard to add a new block into blockchian,
-// thus preventing modification of block
-
-// HashTransactions returns a hash of the transactions in the block
-func (b *Block) HashTransactions() []byte {
-	var txHashes [][]byte
-	var txHash [32]byte
-
-	for _, tx := range b.Transactions {
-		txHashes = append(txHashes, tx.ID)
-	}
-	txHash = sha256.Sum256(bytes.Join(txHashes, []byte{}))
-
-	return txHash[:]
-}
-
-func NewBlock(transactions []*Transaction, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), transactions, prevBlockHash, []byte{}, 0}
-	pow := NewProofOfWork(block)
-	nonce, hash := pow.Run()
-
-	block.Hash = hash[:]
-	block.Nonce = nonce
-
-	return block
-}
-
-// Genesis block: To add a new block we need an existing block,
-// So, in any blockchain, there must be at least one block, such block
-// is called Genesis block
-
-func NewGenesisBlock(coinbase *Transaction) *Block {
-	return NewBlock([]*Transaction{coinbase}, []byte{})
 }
